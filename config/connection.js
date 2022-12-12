72b0643b030b62b2a9a5624ddcc1b958bc470a5d
Lijoy <oommenlijoy@gmail.com>
2022-12-12 11:22:37 +0530

    db updated

--- config/connection.js
+++ config/connection.js
@@ -3,26 +3,26 @@ require('dotenv').config()
 const State={
     db:null
 }
-module.exports.connect = function(done){
-   const url ='mongodb://0.0.0.0:27017'
-   const dbname = 'LJGAMING'
-
-   mongoClient.connect(url,(err,data)=>{
-       if(err) return done(err)
-       State.db = data.db(dbname)
-       done()
-   })
-}
 // module.exports.connect = function(done){
-//     const url = process.env.MONGO_ID
-//     const dbname = 'LJGAMING' 
+//    const url ='mongodb://0.0.0.0:27017'
+//    const dbname = 'LJGAMING'
 
-//     mongoClient.connect(url,(err,data)=>{
-//         if(err) return done(err)
-//         State.db = data.db(dbname)
-//         done()
-//     })
+//    mongoClient.connect(url,(err,data)=>{
+//        if(err) return done(err)
+//        State.db = data.db(dbname)
+//        done()
+//    })
 // }
+module.exports.connect = function(done){
+    const url = process.env.MONGO_ID
+    const dbname = 'LJGAMING' 
+
+    mongoClient.connect(url,(err,data)=>{
+        if(err) return done(err)
+        State.db = data.db(dbname)
+        done()
+    })
+}
 module.exports.get =  function(){
     return State.db
 }
